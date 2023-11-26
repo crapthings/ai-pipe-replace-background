da4e645e8fd900bdb3b953e4a8afdeb77c81457f
crapthings <crapthings@gmail.com>
2023-11-26 20:55:00 +0800

    fix

--- cache.py
+++ cache.py
@@ -2,10 +2,12 @@ import torch
 from transparent_background import Remover
 from RealESRGAN import RealESRGAN
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 print('cache model')
 
 remover = Remover()
-model = RealESRGAN('cuda', scale = 2)
+model = RealESRGAN(device, scale = 2)
 model.load_weights('weights/RealESRGAN_x2.pth', download = True)
 
 print('done')
